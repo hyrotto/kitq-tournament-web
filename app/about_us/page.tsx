196cfbc32f494ddd466f1f9fa760c39b03ce54cf
yamamoto <yamamoto.hiroto932@mail.kyutech.jp>
2024-12-13 00:48:11 +0900

    キャラクターを追加

--- app/about_us/page.tsx
+++ app/about_us/page.tsx
@@ -1,7 +1,17 @@
+import {
+  UIProvider,
+  NativeImage
+} from "@yamada-ui/react"
+
 export default async function Home() {
     return (
       <div>
         <h1>キャラクター紹介</h1>
+        <NativeImage
+          src="/cute_cat.png" 
+          h = "300"
+        />
+        <p>ジャック君 (17)</p>
       </div>
     );
   }
