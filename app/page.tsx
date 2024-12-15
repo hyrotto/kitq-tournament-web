919dca6ac6bab6617b5ea6adcab433554699203e
yamamoto <bycmbog@gmail.com>
2024-12-15 22:03:17 +0900

    メインイラストを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import styles from "./page.module.css";
+import {
+  NativeImage
+} from "@yamada-ui/react"
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>root</h1>
+      h1
+      <NativeImage
+        src="大会メインイラスト_モック.png"
+        h = "600"
+      />
     </div>
   );
 }
