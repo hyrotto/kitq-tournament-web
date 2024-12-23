6d591c2d82a70fa8074683933ba4e89f8d70c527
yamamoto <bycmbog@gmail.com>
2024-12-24 01:14:51 +0900

    エントリーページとエントリーボタンを追加

--- app/entry/page.tsx
+++ app/entry/page.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import styles from "./page.module.css"
+import {
+  Link
+} from "@yamada-ui/react"
 
 export default async function Entry() {
     return (
-      <div>
-        <h1>エントリー</h1>
+      <div className={styles.container}>
+        <h1 className={styles.TitleText}>エントリー</h1>
+        <div className={styles.DescriptionText}>
+          エントリーは<span>XX月XX日(金)21:30</span>より開始いたします。
+        </div>
+        ↓エントリーフォームはこちら
+        <Link className={styles.EntryButton}>
+          エントリーフォーム
+        </Link>
       </div>
     );
   }
