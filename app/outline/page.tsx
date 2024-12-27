66b028ed22f5d86f406916f2af56cf3755d81cd1
hyrotto <bycmbog@gmail.com>
2024-12-28 03:55:02 +0900

    企画書pdf要素を追加

--- app/outline/page.tsx
+++ app/outline/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./page.module.css"
 
 export default async function Outline() {
     return (
-      <div>
-        <h1>大会概要</h1>
+      <div className={styles.container}>
+        <h1 className={styles.TitleText}>大会概要</h1>
+        <h1 className={styles.SubTitleText}>本大会企画書</h1>
+        <iframe
+          src="/企画書草案_モック.pdf"
+        ></iframe>
+        <h1 className={styles.SubTitleText}>本大会企画書</h1>
+
       </div>
     );
   }
