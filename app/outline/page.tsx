c33d1e57f01b318a36b89f56e183185290f24f3d
yamamoto <bycmbog@gmail.com>
2025-01-31 16:19:35 +0900

    例題を更新

--- app/outline/page.tsx
+++ app/outline/page.tsx
@@ -46,41 +46,34 @@ export default async function Outline() {
           <Tbody>
             <Tr>
               <Td className={styles.tableTd}>
-                「鼠径」「大腿」「椎間板」などの種類がある、体内の臓器などが本来あるべき位置から突出した状態のことを、ラテン語で「飛び出す」という意味の言葉で何というでしょう？
+              電力が必須の楽器ではあるが発音自体に電力は使われていないため「電気楽器」には分類されない、大型の鉄琴に電気式の共鳴管を付けた楽器を何というでしょう？
               </Td>
-              <Td className={styles.tableTd}>ヘルニア</Td>
+              <Td className={styles.tableTd}>ビブラフォン</Td>
             </Tr>
             <Tr>
               <Td className={styles.tableTd}>
-                1895年に「ヒトの心電図の形」を発表し、1924年には「心電図法の発見」の功績によりノーベル生理学・医学賞を受賞したオランダの生理学者は誰でしょう？
+              特にプロスポーツでは怪我や欠場などのアクシデントにも柔軟に対応できるため重宝される、チームスポーツにおいて複数のポジションを守ることの出来る選手を何というでしょう？
               </Td>
-              <Td className={styles.tableTd}>ウィレム・アイントホーフェン</Td>
+              <Td className={styles.tableTd}>ユーティリティプレイヤー</Td>
             </Tr>
             <Tr>
               <Td className={styles.tableTd}>
-                1980年代にモトローラ社の技術者ビル・スミスによって開発された、100万回の作業を実施しても不良品の発生率を3.4回に抑えるという品質管理手法を何というでしょう？
+              赤色や青色、茶色などの顔料を用いて筋肉や血管を大げさに表している、色によって役割や性格を表す歌舞伎の特徴的な化粧法を何というでしょう？
               </Td>
-              <Td className={styles.tableTd}>シックス・シグマ</Td>
+              <Td className={styles.tableTd}>隈取</Td>
             </Tr>
             <Tr>
               <Td className={styles.tableTd}>
-              イエス・キリストが礫にされた時にその釘を引き抜こうとしたという伝承があり、その嘴の形状は物事が食い違って思うようにならないことの喩えに使われる鳥は何でしょう？
+              昨年（2024年）5月には日本平（にほんだいら）パーキングエリアに出店した、トレーニングの他にも様々なサービスを提供している、RIZAP株式会社が運営しているコンビニジムは何でしょう？
               </Td>
-              <Td className={styles.tableTd}>イスカ
+              <Td className={styles.tableTd}>chocoZAP
               </Td>
             </Tr>
             <Tr>
               <Td className={styles.tableTd}>
-              ウェブブラウザ「Google Chrome」のアイコンにおいて、真ん中の丸の色は何色でしょう？
+              借りたものを持ち主に戻すことを表す2 字熟語「返済」の「返（へん）」に送り仮名「す」をつけると「かえす」と読みますが、「済（さい）」に送り仮名「す」をつけると何と読むでしょう？
               </Td>
-              <Td className={styles.tableTd}>青</Td>
-            </Tr>
-            <Tr>
-              <Td className={styles.tableTd}>
-              クマリ寺が位置することから「カニヤー・クマリ」とも呼ばれ、ベンガル湾、アラビア海、インド洋を一望することができる、インド最南端の岬は何でしょう？
-              </Td>
-              <Td className={styles.tableTd}>コモリン岬
-              </Td>
+              <Td className={styles.tableTd}>わたす【○：なす】</Td>
             </Tr>
           </Tbody>
         </NativeTable>
