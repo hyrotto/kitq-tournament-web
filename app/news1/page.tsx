c32bd591b5b793c01581e0d83c90976b0996719d
yamamoto <bycmbog@gmail.com>
2025-01-31 17:14:11 +0900

    誤字を修正

--- app/news1/page.tsx
+++ app/news1/page.tsx
@@ -6,10 +6,10 @@ export default async function NewsFirstPage() {
   return (
     <div className={styles.container}>
         <h1 className={styles.TitleText}>クイズ大会HP公開のお知らせ</h1>
-        <div className={styles.DateText}>2024年1月31日</div>
+        <div className={styles.DateText}>2025年1月31日</div>
       <div className={styles.DescriptionText}>
         <p>
-        この度、King&Queen杯の会の公式ホームページを公開いたしました！
+        この度、King&Queen杯の公式ホームページを公開いたしました！
         </p>
         <p>
         大会に関する最新情報やエントリー開始など、各種情報を発信してまいります。
