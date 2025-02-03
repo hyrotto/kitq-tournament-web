e7cd20bc06780d53a545beab48dc22e6f649b921
hyrotto <bycmbog@gmail.com>
2025-02-03 18:30:49 +0900

    react-pdf-viewerにローカリゼーションファイルをインストール

--- app/outline/pdfView.tsx
+++ app/outline/pdfView.tsx
@@ -2,16 +2,17 @@
 import React from "react";
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-
+import { Worker, Viewer, LocalizationMap } from '@react-pdf-viewer/core';
+import jp_JP from '@react-pdf-viewer/locales/lib/jp_JP.json';
 function PDFviewer() {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
+    
     return(
         <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}>
             <Viewer
                 fileUrl="/King&Queen杯_企画書.pdf"
                 plugins={[defaultLayoutPluginInstance]}
+                localization={jp_JP as unknown as LocalizationMap}
             />
         </Worker>
     )
