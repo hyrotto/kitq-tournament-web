eb2f72a536148a4cba940d0e94f5b56c21c0e029
hyrotto <bycmbog@gmail.com>
2025-02-03 19:09:37 +0900

    pdf-viewerにcssを適用

--- app/outline/pdfView.tsx
+++ app/outline/pdfView.tsx
@@ -1,14 +1,16 @@
 "use client";
 import React from "react";
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { ScrollMode } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import { Worker, Viewer, LocalizationMap } from '@react-pdf-viewer/core';
 import jp_JP from '@react-pdf-viewer/locales/lib/jp_JP.json';
+import styles from './pdfView.module.css';
 function PDFviewer() {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
     
     return(
-        <div className="no-google-font">
+        <div className={styles.nogooglefont}>
             <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}>
                 <Viewer
                     fileUrl="/King&Queen杯_企画書.pdf"
