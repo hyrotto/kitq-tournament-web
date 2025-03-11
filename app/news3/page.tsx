8683d4574e471e3338ac9e7ce801c0de276c0d58
yamamoto <bycmbog@gmail.com>
2025-03-11 16:12:25 +0900

    エントリー締め切りのニュースを修正

--- app/news3/page.tsx
+++ app/news3/page.tsx
@@ -5,16 +5,16 @@ import { Link } from "@yamada-ui/react";
 export default async function NewsPage() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.TitleText}>大会エントリーを締め切りました</h1>
+      <h1 className={styles.TitleText}>エントリーを締め切りました</h1>
       <div className={styles.DateText}>2025年3月11日</div>
       <div className={styles.DescriptionText}>
         <p>
           3月10日の24時をもって大会エントリーを締め切りました。
         </p>
         <p>
-          46名ものエントリー、本当にありがとうございます。
+          たくさんのエントリー、本当にありがとうございます。
         </p>
-        <p>
+        <p> 
           大会当日皆さんにお会いできることを、キングくんとクイーンちゃんをはじめ運営一同楽しみにしております！ 
         </p>
       </div>
